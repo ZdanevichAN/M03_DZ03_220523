724877647190d68b5417d0bbeb88a5a2d4d60231
Alecsander Zdanevich <alecsdjkzda@gmail.com>
2023-05-22 12:33:46 +0300

    ok

---
+++ task19_DZ/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int CheckNumOfChar(int some_num)
+{
+    int prove = some_num%10000;
+    if (prove==0) {Console.WriteLine($"Введено число{some_num}");}
+    else          {Console.WriteLine("-число не пятизначное");    } 
+    return some_num;
+}
+int ReverseInt(int some_num)
+{
+    int reverseNum = 0;
+    while(some_num!=0)
+    {
+        reverseNum=reverseNum*10+some_num%10;
+        some_num/=10;
+    }
+    Console.WriteLine($"{reverseNum}");
+    return reverseNum;
+}
+System.Console.Write("Введите число: ");
+int user_num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Пользователем введено число {user_num}");
+if (user_num == ReverseInt(CheckNumOfChar(user_num))) {System.Console.WriteLine($"Это число{user_num} -  палиндром");}
+else                                                  {System.Console.WriteLine($"Это число{user_num} - не полиндром");}
+
+// System.Console.WriteLine($"Это число{user_num} -  палиндром");
+// System.Console.WriteLine($"Это число{user_num} - не полиндром");
---
+++ task21_DZ/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.WriteLine("Введите координаты точки A:");
+    double ax = double.Parse(Console.ReadLine());
+    double ay = double.Parse(Console.ReadLine());
+    double az = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B:");
+    double bx = double.Parse(Console.ReadLine());
+    double by = double.Parse(Console.ReadLine());
+    double bz = double.Parse(Console.ReadLine());
+    double distance = CalculateDistance(ax, ay, az, bx, by, bz);
+Console.WriteLine("Расстояние между точкой A и точкой B: " + distance.ToString("F2"));
+static double CalculateDistance(double ax, double ay, double az, double bx, double by, double bz)
+{
+    double distance = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+    return distance;
+}
